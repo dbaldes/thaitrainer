0a4bbe15bf535624e53d765d2d75a12fb57895cf
Daniel Baldes <daniel@baldes.name>
2017-03-05 21:59:30 +0100

    guess mode

--- src/app/consonant.component.ts
+++ src/app/consonant.component.ts
@@ -20,18 +20,35 @@ export class ConsonantComponent implements OnInit {
 
   characterClass: string;
 
+  guessingPhonetic = false;
+  guessingClass = true;
+
+  showClass = true;
+  showPhonetic = true;
+ 
+  wrongClass = false;
+  rightClass = false;
+
+  wrongPhonetic = false;
+  rightPhonetic = false;
+
   constructor(
     private consonantService: ConsonantService,
     private route: ActivatedRoute,
     private location: Location) { }
 
   setConsonant(consonant: Consonant) {
+    this.showClass = !this.guessingClass;
+    this.showPhonetic = !this.guessingPhonetic;
+    this.wrongClass = false;
+    this.rightClass = false;
+    this.wrongPhonetic = false;
+    this.rightPhonetic = false;
+
     this.consonant = consonant;
     this.characterClass = 
-        consonant.charclass == 'l'
-          ? 'low' 
-          : consonant.charclass == 'm'
-            ? 'medium'
+        consonant.charclass == 'l' ? 'low' 
+          : consonant.charclass == 'm' ? 'middle'
             : 'high';
   }
 
@@ -39,6 +56,25 @@ export class ConsonantComponent implements OnInit {
     this.route.params
       .switchMap((params: Params) => this.consonantService.getConsonant(+params['id']))
       .subscribe(consonant => this.setConsonant(consonant));
+  }
+
+
+  guessClass(guess: string): void {
+    if (guess === this.consonant.charclass) {
+      this.rightClass = true;
+    } else {
+      this.wrongClass = true;
+    }  
+    this.showClass = true;
+  }
+
+  guessPhonetic(guess: string): void {
+    if (guess === this.consonant.roman.substring(0,1)) {
+      this.rightPhonetic = true;
+    } else {
+      this.wrongPhonetic = true;
+    }
+    this.showPhonetic = true;
   }
 
   goBack(): void {
